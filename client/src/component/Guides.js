8a0e03187450d45c09f47d2de7b822efefab0a17
netcohen <netcohen@101.org.il>
2022-07-11 21:42:50 +0300

    css changes

---
+++ client/src/component/Guides.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Guides extends React.Component {
+
+render() {
+return ( 
+ <h3>Guides</h3> 
+)
+}
+}
+
+export default Guides
--- client/src/component/Tabs.js
+++ client/src/component/Tabs.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Forum from "./Forum";
 import Contact from "./Contact";
 import Personal from "./Personal";
+import Guides from "./Guides";
 
 
 function Tabs() {
@@ -17,7 +18,7 @@ function Tabs() {
         <div className="header">
             <div className="bloc-tabs">
                 <div className={toggleState === 1 ? "tabs active-tabs" : "tabs"} onClick={() => toggleTab(1)}>forum</div>
-                <div className={toggleState === 2 ? "tabs active-tabs" : "tabs"} onClick={() => toggleTab(2)}> Tab 2</div>
+                <div className={toggleState === 2 ? "tabs active-tabs" : "tabs"} onClick={() => toggleTab(2)}>Guides</div>
                 <div className={toggleState === 3 ? "tabs active-tabs" : "tabs"} onClick={() => toggleTab(3)}>Personal-area</div>
                 <div className={toggleState === 4 ? "tabs active-tabs" : "tabs"} onClick={() => toggleTab(4)}>Contact</div>
             </div>
@@ -27,7 +28,7 @@ function Tabs() {
                     <div><Forum /> </div>
                 </div>           
                 <div className={toggleState === 2 ? "content active-content" : "content"}>
-                    <div></div>
+                    <div><Guides /></div>
                 </div>
                 <div className={toggleState === 3 ? "content active-content" : "content"}>
                     <div><Personal/></div>
